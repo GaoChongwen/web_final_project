a4bdbe686910b5270f61286bd4a287e0a0658d30
gaosaroma <gaosaroma@gmail.com>
2019-06-23 11:50:43 +0800

    update name & add PPT

--- js/model.js
+++ js/model.js
@@ -2,6 +2,7 @@ window.model = {
     data: {
         items: [
             // {msg:'', completed: false, timeLimited: false, alerted: false, isTd: false, dateStr: ''}
+            // dateStr:'yyyy.mm.dd-yyyy.mm.dd'
         ],
         msg: '',
         filter: 'All'
